4ff1ec2a28ab7ac301b9ac91de290fc61490a457
DominikEnder <dai2125@students.fhv.at>
2023-11-29 13:02:02 +0100

    update javascript

--- app.js
+++ app.js
@@ -12,27 +12,27 @@ app.use(express.static('public'));
 app.use('/static', express.static('public'));
 
 // API-Endpunkt für JSON
-app.get('/api/pinRed', (req, res) => {
-    fs.readFile('./public/pinRed.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error("Fehler beim Lesen der Datei:", err);
-            res.status(500).json({ error: "Fehler beim Lesen der Datei" });
-            return;
-        }
-        res.json(JSON.parse(data));
-    });
-});
+// app.get('/api/pinRed', (req, res) => {
+//     fs.readFile('./public/pinRed.json', 'utf8', (err, data) => {
+//         if (err) {
+//             console.error("Fehler beim Lesen der Datei:", err);
+//             res.status(500).json({ error: "Fehler beim Lesen der Datei" });
+//             return;
+//         }
+//         res.json(JSON.parse(data));
+//     });
+// });
 // variabel übergeben anstatt Pfade (/api/pin/{color/id) URL kannst du auslesen app.get(users/:userid  expressjs.com
-app.get('/api/pinBlue', (req, res) => {
-    fs.readFile('./public/pinBlue.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error("Fehler beim Lesen der Datei:", err);
-            res.status(500).json({ error: "Fehler beim Lesen der Datei" });
-            return;
-        }
-        res.json(JSON.parse(data));
-    });
-});
+// app.get('/api/pinBlue', (req, res) => {
+//     fs.readFile('./public/pinBlue.json', 'utf8', (err, data) => {
+//         if (err) {
+//             console.error("Fehler beim Lesen der Datei:", err);
+//             res.status(500).json({ error: "Fehler beim Lesen der Datei" });
+//             return;
+//         }
+//         res.json(JSON.parse(data));
+//     });
+// });
 
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000/');
